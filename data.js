025d27c731a1788502cab5ef6ae420e6a6ba78ec
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 08:35:32 -0600

    sum

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-viscom",
+      "name": "VisCom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.5854191978345344,
+        "pitch": 0.2551249367363351,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.0297783268954621,
+          "pitch": 0.05525934479122441,
+          "title": "VisCom4",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.72263992932751,
+          "pitch": 0.0014862895427949496,
+          "title": "VisCom3",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3143985530221478,
+          "pitch": 0.04512935216146374,
+          "title": "VisCom1",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.42484642350413,
+          "pitch": 0.04738124048525094,
+          "title": "VisCom2",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.20051123773825452,
+          "pitch": 0.08477991781941441,
+          "title": "VisCom5",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
