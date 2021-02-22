67087abc1473c5dc14708040b249929dede96aa3
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 08:54:07 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,43 +34,43 @@ var APP_DATA = {
           "yaw": 1.2979928408131958,
           "pitch": 0.039673790003728726,
           "title": "Courses",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1fBBKvrp6P5dGESUOM3U3_WWF42arPEZ2/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.416265602365666,
           "pitch": 0.04252392218092105,
           "title": "TSA",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1fFgvjrh8xU6QPyVFSijQTYTqIXh6DoOh/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.726750477348702,
           "pitch": -0.006473525788956991,
           "title": "Projects",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1_fuZXM3jC6tfOL3nFZBYR0RoULyMNdYe/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.0196153321143893,
           "pitch": 0.0010840851534688056,
           "title": "Technology",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1VhCq8N8TSmJU93I0dNEuEdVgjswsLGpU/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.04543595640178566,
           "pitch": -0.004696652167131887,
           "title": "LAUNCH",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1bmO2FXCd44kBpG5AXi4iYtBb7fraHf5v/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 1.3821965836003614,
           "pitch": 0.6208005405638559,
           "title": "Introduction",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1oZznqoThBKRBUzlrjqvyvDZiILBqlrLM/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 0.4361012510686386,
           "pitch": 0.006753445854315032,
           "title": "Why VisCom?",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1nCCMpYi6UIOv5jDLEGxdFivkfn7G3N8F/preview" width="640" height="480"></iframe>'
         }
       ]
     }
